81b4dc2b4fc9bc70fa929ec7a1bd50043a361277
Artem Bezzubets <artem.bezzubets@gmail.com>
2016-01-21 16:13:00 -0500

    Hided Linux machines from GUI

--- ChangeGen_v2/ChangeGen_v2/Model/CoreConnector.cs
+++ ChangeGen_v2/ChangeGen_v2/Model/CoreConnector.cs
@@ -47,11 +47,10 @@ namespace ChangeGen_v2
             {
                 if ((agent.AgentType != AgentType.EsxServer) && (agent.AgentType != AgentType.EsxVirtualMachine))
                 {
-                    serversList.Add(new Server(agent.Descriptor.HostUri.Host, agent.DisplayName, agent.RepositoryName));
+                    if (!_coreClient.AgentsManagement.GetAgentMetadataById(agent.Id.ToString()).OSVersion.ToString().Contains("Linux"))
+                        serversList.Add(new Server(agent.Descriptor.HostUri.Host, agent.DisplayName, agent.RepositoryName));
                 }
-
             }
-
             return serversList;
         }
 
